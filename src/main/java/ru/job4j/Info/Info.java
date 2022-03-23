c04053e698f4a081ce7804f743cd0d0c3e53b6a1
katring28 <mactoob@mail.ru>
2022-03-23 21:59:55 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info/Info.java
+++ src/main/java/ru/job4j/Info/Info.java
@@ -2,6 +2,6 @@ package ru.job4j.Info;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("28.08.1991");
+        System.out.println("23.03.2021");
     }
 }
