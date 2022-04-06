8b237bc6e1e9132121b9f8ef6811ec0bb74d07f8
katring28 <mactoob@mail.ru>
2022-04-06 22:34:16 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        int year = 2022;
+        String id = idea + " But I am a newbie. " + year;
+        System.out.println(id);
+        System.out.println(idea);
+    }
+}
