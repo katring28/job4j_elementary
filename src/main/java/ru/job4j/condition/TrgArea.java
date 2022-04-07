6e8a153b58b6c3504a37ff34b52414a37bb68b2e
katring28 <mactoob@mail.ru>
2022-04-07 13:26:22 +0300

    7. Расстояние между точками в системе координат [#188 #285012]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -3,9 +3,8 @@ package ru.job4j.condition;
 public class TrgArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double s = p * (p - a) * (p - b) * (p - c);
-        double ss =  Math.sqrt(s);
-        return ss;
+        double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return s;
     }
 
     public static void main(String[] args) {
